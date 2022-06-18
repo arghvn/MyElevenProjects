6f6c92aaf22343ff913bb1bac687495f7b0643be
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-18 17:54:38 +0430

    outputs

--- first_project/Text_Templates.go
+++ first_project/Text_Templates.go
@@ -53,3 +53,13 @@ func main() {
 			"C#",
 		})
 }
+
+// output:
+// Value: some text
+// Value: 5
+// Value: [Go Rust C++ C#]
+// Name: Jane Doe
+// Name: Mickey Mouse
+// yes
+// no
+// Range: Go Rust C++ C#
