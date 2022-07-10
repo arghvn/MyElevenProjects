19cf0ebd20dd71bf8b034c20b9a8fec01695c39f
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-10 10:14:43 +0430

    installation

--- third_project/main.go
+++ third_project/main.go
@@ -1,1 +1,4 @@
 // building bookstrore management system
+// so in terminal , cd third project , go mod init github.com/arghvn/main.go , go get "github.com/jinzhu/gorm" , go get "github.com/jinzhu/gorm/dialects/mysql"
+// go get "github.com/gorilla/mux"
+
