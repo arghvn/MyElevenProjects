a8247e8e05600cf6fa651ba171013280bf8f6017
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-17 19:04:51 +0430

    keep on coding

--- second_project/The_fourth_project/main.go
+++ second_project/The_fourth_project/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"os"
+)
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"github.com/shomalil/slacker"
+// )
+
+func PrintCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+
+}
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-310246501329-2253154075362-5bewxhgvdaoCpRKivBSeG4P4")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A027MAS7CC-2246395933878-354ffc9a0aad7282fca446b1999989f75a0b8d48e5284fedid86e59e18614118")
+
+	// create bot
+	bot := Slacker.NewCilent(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go PrintCommandEvents(bot.CommandEvents())
+}
