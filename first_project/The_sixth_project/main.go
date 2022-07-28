808073b610f28cb6effc7ed53a3e5a68f64ed28f
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-28 17:54:08 +0430

    start coding

--- first_project/The_sixth_project/main.go
+++ first_project/The_sixth_project/main.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"net"
+// 	"os"
+// 	"strings"
+// 	)
+
+func main() {
+
+	// make a variable by bufio package
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain,hasMX,hasSPF,spfRecord,hasDMARC,dmarRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+	// this function just takes a domain
+
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarRecord string
+
+	// using net package to lookup and related function that returns error for us
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error:%v\n", err)
+	}
+
+	for _, Record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = Record
+			break
+		}
+	}
+}
--- first_project/The_sixth_project/readme.md
+++ first_project/The_sixth_project/readme.md
@@ -1,7 +1,12 @@
 building an email verifier tool 
+checking that an email is true or fake
 
-after making this code if we write go run main.go in terminal we have a statement domain,hasMX,hasSPF,spfRecord,hasDMARC,dmarRecord thath says 
+after making this code if we write go run main.go in terminal we have a statement
+domain,hasMX,hasSPF,spfRecord,hasDMARC,dmarRecord thath says 
 when you enter a domain name right now it will give you the doamin name and check it and mx records...
 and by checking these then we will know if that domain is right from which that email id is come out from to or not
 so youll just check the domain
-
+in the next part we enter an email record to check like mailchimp.com it returns a statement for us like 
+mailchimp.com,true,false,,true,v=DMARC1; p = reject; rua = mailto:19ezfriw@ag.dmarcian.com; ruf = mailto:19ezfriw@fr.dmarcian.com
+that means :
+mailchimp.com is the domain , hasMX is true and etc(spfRecord is blank and no spfRecord)
